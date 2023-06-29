fe4f2c5079f8d6ffcca96c4ab74f7dd51d1d79ac
Krapp Ramiro <krappramiro@disroot.org>
2023-06-29 14:59:07 -0300

    add preliminary file uploading

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, make_response
 from app.forms import TestsForm
 
 app.config["SECRET_KEY"] = "your_secret_key_here"
@@ -12,5 +12,12 @@ def index():
 
 @app.route("/process-form/", methods=["GET", "POST"])
 def process_form():
-    form_data = request.get_json()
-    return jsonify(form_data)
+    form_data = {}
+    for key, value in request.form.items():
+        form_data[key] = value
+
+    files = request.files
+    for key, file in files.items():
+        form_data[key] = file.filename
+    print(form_data)
+    return "Files processed successfully"
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,58 +1,79 @@
+// Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize form count and get the "Add Form" button and form fields container
-  var formCount = 1;
-  var addFormButton = document.getElementById("add-form-btn");
-  var formFieldsContainer = document.getElementById("form-fields");
+  let formCount = 0;
+  let addFormButton = document.getElementById("add-form-btn");
+  let formFieldsContainer = document.getElementById("form-fields");
+  let noFormsMessage = document.getElementById("no-forms-message");
 
   // Event listener for adding a new form field
   addFormButton.addEventListener("click", function () {
     // Create a new form field div
-    var formField = document.createElement("div");
+    let formField = document.createElement("div");
     formField.classList.add("form-field");
     formField.innerHTML = `
       <hr>
       <h3>Form ${formCount + 1}</h3>
       <label for="name${formCount + 1}">Name</label>
-      <input type="text" name="name${formCount + 1}"
-             id="name${formCount + 1}" required>
+      <input type="text" name="name${formCount + 1}" id="name${
+      formCount + 1
+    }" required>
+      <label for="description${formCount + 1}">Description</label>
+      <input type="text" name="description${formCount + 1}" id="description${
+      formCount + 1
+    }" required>
+      <label for="file${formCount + 1}">File</label>
+      <input type="file" name="file${formCount + 1}" id="file${
+      formCount + 1
+    }" required>
     `;
 
     // Append the new form field to the container
     formFieldsContainer.appendChild(formField);
     formCount++;
+
+    // Hide the "No Forms" message when a form is added
+    noFormsMessage.style.display = "none";
   });
 
   // Get the dynamic form element
-  var dynamicForm = document.getElementById("dynamic-form");
+  // ------------------ SUBMITTING THE FORM ------------------
+  let dynamicForm = document.getElementById("dynamic-form");
   dynamicForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // Create an object to store form data
-    var formData = {};
+    // Create a new FormData object to store form data
+    var formData = new FormData();
 
     // Get all form field elements
-    var formFields = document.getElementsByClassName("form-field");
+    let formFields = document.getElementsByClassName("form-field");
+    console.log(`When submitting the form, found ${formFields.length} forms`);
     Array.from(formFields).forEach(function (field, index) {
-      // Get the input element within each form field
-      var input = field.querySelector("input");
-      // Assign the input value to the corresponding property in the formData object
-      formData[`name${index + 1}`] = input.value;
+      // Get the input elements within each form field
+      let nameInput = field.querySelector('input[name^="name"]');
+      let descriptionInput = field.querySelector('input[name^="description"]');
+      let fileInput = field.querySelector('input[name^="file"]');
+      console.log(`Adding the form ${index} with data:
+			name: ${nameInput.value}
+			name: ${descriptionInput.value}
+			name: ${fileInput.files[0]}
+		`);
+      // Append the input values to the formData object
+      formData.append(`name${index + 1}`, nameInput.value);
+      formData.append(`description${index + 1}`, descriptionInput.value);
+      formData.append(`file${index + 1}`, fileInput.files[0]);
     });
 
-    // Send the form data as JSON to the server using the Fetch API
+    // Send the form data (including files) to the server using the Fetch API
     fetch("/process-form", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(formData),
+      body: formData,
     })
       .then(function (response) {
-        return response.json();
+        return response;
       })
       .then(function (data) {
-        console.log(data);
+        console.log(`Response:\n${data}`);
         // Handle the response data as needed
       })
       .catch(function (error) {
